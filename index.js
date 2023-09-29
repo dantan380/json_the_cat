2c3265cc9d0946248d27e7ca8d1cf56e707d4dcd
Daniel Tan <danieltan380@gmail.com>
2023-09-28 18:44:46 -0700

    Added comments explaining the function call

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const { fetchBreedDescription } = require('./breedFetcher');
 
 const breedName = process.argv[2];
 
-fetchBreedDescription(breedName, (error, desc) => {
-	if(error){
-		console.log("Error:", error);
-	} else {
-		console.log(desc);
-	}
+fetchBreedDescription(breedName, (error, desc) => { //Call fetch and pass it an anonymous function that takes two parameters.
+  if (error) {
+    console.log("Error:", error); //If error, console.log it.
+  } else {
+    console.log(desc); //If not, console.log desc, which should be the data received from the API request.
+  }
 });
