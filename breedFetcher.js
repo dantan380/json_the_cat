6df84f3bffb408e8dc56ec1540581fbe84353bdc
Daniel Tan <danieltan380@gmail.com>
2023-09-28 18:41:55 -0700

    Refactored to fetchBreedDescription function which takes a breedname and a callback. Callback is defined in index file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,4 +1,5 @@
-const args = process.argv.slice(2);
+const request = require('request');
+/*const args = process.argv.slice(2);
 const request = require('request');
 request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
   if (error) {
@@ -11,4 +12,24 @@ request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, respo
       console.log(data[0].description);
     }
   }
-});
+});*/
+
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  console.log(url);
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);	//If there is an error, call the callback function and pass it the error.
+    } else {
+      const data = JSON.parse(body);
+      if (data.length > 0) {
+        const description = data[0].description;
+        callback(null, description); //If no error, call the callback function and pass it the description pulled from the api request.
+      } else {
+        callback('Breed not found', null); //If user did not type a breed name or mispelled, call callback and pass the string.
+      }
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
